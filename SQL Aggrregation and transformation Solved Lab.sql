0907787739e25a950a89654da030c41e1b45b28d
fer-nandes13 <129901835+fer-nandes13@users.noreply.github.com>
2023-10-26 00:06:47 +0200

    Solved lab

--- SQL Aggrregation and transformation Solved Lab.sql
+++ SQL Aggrregation and transformation Solved Lab.sql
@@ -3,15 +3,6 @@
 # Lab | SQL Data Aggregation and Transformation
 
 
-
-
-
-## Introduction
-
-
-
-Imagine you work at a movie rental company as an analyst. By using SQL in the challenges below, you are required to gain insights into different elements of its business operations.
-USE sakila
 ## Challenge 1
 
 1. You need to use SQL built-in functions to gain insights relating to the duration of movies:
@@ -46,9 +37,12 @@ USE sakila;
 SELECT *
 FROM film;
 SELECT length
+#1.1 Determine the shortest and longest movie durations and name the values as max_duration and min_duration.
 FROM sakila.film;
 SELECT MAX(length) AS 'longest_movie_duration',  MIN(length) AS 'shortest_movie_duration'
 FROM sakila.film;
+
+#1.2. Express the average movie duration in hours and minutes. Don't use decimals
 SELECT 
     CONCAT(
         FLOOR(AVG(length) / 60), ' hours ',
@@ -56,33 +50,55 @@ SELECT
     ) AS average_duration
 FROM 
     sakila.film;
+#2.1 Calculate the number of days that the company has been operating
+SELECT DATEDIFF(MAX(rental_date), MIN(rental_date)) AS days_operating
+FROM sakila.rental;
+# company has been operating for 266 days
 
+#2.2 Retrieve rental information and add two additional columns to show the month and weekday of the rental. Return 20 rows of results.
 SELECT rental_id,rental_date,EXTRACT(MONTH FROM rental_date) AS rental_month,EXTRACT(DAY FROM rental_date) AS weekday_rental
 FROM rental
 LIMIT 20;
 
+#You need to ensure that customers can easily access information about the movie collection. 
+#To achieve this, retrieve the film titles and their rental duration. 
+#If any rental duration value is NULL, replace it with the string 'Not Available'. Sort the results of the film title in ascending order.
 SELECT SUM(ISNULL(rental_duration))
-FROM sakila.film;
+FROM sakila.film; #Nulls verification
+
+#3You need to ensure that customers can easily access information about the movie collection. 
+#To achieve this, retrieve the film titles and their rental duration. Sort the results of the film title in ascending order.
+# If any rental duration value is NULL, replace it with the string 'Not Available
+#Sort the results of the film title in ascending order
 
 SELECT title AS film_title,IFNULL(rental_duration, 'Not Available') AS 'rental_duration'
-FROM sakila.film;
+FROM sakila.film
+ORDER BY rental_duration ASC;
 
+#CHALLENGE 2
+#1.1 The total number of films that have been released.
 SELECT COUNT(*) AS 'total_films_released'
 FROM film;
+#1.2 The number of films for each rating.
 SELECT rating, COUNT(*) AS number_of_films_for_each_rating
 FROM sakila.film
 GROUP BY rating;
-
+#1.3 The number of films for each rating, sorting the results in descending order of the number of films. 
+#This will help you to better understand the popularity of different film ratings and adjust purchasing decisions accordingly.
 SELECT rating, COUNT(*) AS 'number_of_films_for_each_rating'
 FROM sakila.film
 GROUP BY rating
 ORDER BY COUNT(*) DESC;
 
+#2 Using the film table, determine
+#2.1 The mean film duration for each rating, and sort the results in descending order of the mean duration. 
+#Round off the average lengths to two decimal places. This will help identify popular movie lengths for each category.
 SELECT rating, ROUND(AVG(length),2) AS 'average_film_duration'
 FROM sakila.film
 GROUP BY rating
 ORDER BY COUNT(*) DESC;
 
+#2.2 Identify which ratings have a mean duration of over two hours in order to help select films for customers who prefer longer movies.
 SELECT rating, ROUND(AVG(length),2) AS 'average_film_duration'
 FROM sakila.film
 GROUP BY rating 
